2dee5380024a5dff370fe84f76475f6da13ee21b
GreenT <GreenT@MacBook-Pro.local>
2020-06-23 22:56:26 +0900

    Question1

---
+++ prePbl/src/Register.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register {
+    public static void main(String[] args) {
+
+        //Calcクラス（計算する）を生成
+        Calc calc = new Calc();
+
+        calc.setNum(1,2);
+        calc.setNum(2,5);
+
+        System.out.println(calc.sum(1,2));
+
+    }
+}
